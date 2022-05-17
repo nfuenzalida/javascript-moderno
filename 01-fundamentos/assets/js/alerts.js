892269748e9f74e6ae93162c9a76647ae018d56e
Nicolas Fuenzalida <na.fuenzalida.contreras@gmail.com>
2022-05-17 19:17:34 -0400

    prompt, confirm y alert

---
+++ 01-fundamentos/assets/js/alerts.js
@@ -0,0 +1,10 @@
+alert('Hola Mundo');
+
+let nombre = prompt('¿Cual es tu nombre?');
+console.log(nombre);
+console.log('*** ' + nombre + ' ***');
+
+const seleccion = confirm('¿Está seguro de borrar?');
+console.log(seleccion);
+
+//console.log(global);
