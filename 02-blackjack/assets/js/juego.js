1342b878c33af618fff10fc610581fa951dad7d0
Nicolas Fuenzalida <na.fuenzalida.contreras@gmail.com>
2022-06-08 19:58:58 -0400

    pedir carta

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -4,7 +4,6 @@ const tipos         =   ['C','D','H','S'];
 const especiales    =   ['A','J','Q','K']; 
 
 const crearDeck = () => {
-
     for(let i =2; i <= 10; i++ ) {
         for(let tipo of tipos) {
             deck.push(i + tipo);
@@ -27,13 +26,23 @@ const crearDeck = () => {
 crearDeck();
 
 const pedirCarta = () => {
+    if(deck.length === 0) {
+        throw 'No hay cartas en el deck';
+    }
 
     const carta = deck.pop();
 
     console.log(deck);
     console.log(carta);
     return carta;
-
 }
 
 pedirCarta();
+
+const valorCarta = (carta) => {
+    const valor = carta.substring(0,carta.length-1);
+    return (isNaN(valor)) ? (valor === 'A') ? 11 : 10 : valor * 1;
+}
+
+const valor = valorCarta(pedirCarta());
+console.log(valor); 
