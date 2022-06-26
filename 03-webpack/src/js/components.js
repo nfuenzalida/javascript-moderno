04dc56d2ae0a19357209229652efbddc0562f5af
Nicolas Fuenzalida <na.fuenzalida.contreras@gmail.com>
2022-06-26 10:12:26 -0400

    necesidad de webpack

---
+++ 03-webpack/src/js/components.js
@@ -0,0 +1,9 @@
+
+export const saludar = ( nombre = 'sin nombre' ) => {
+    console.log('Creando etiqueta h1');
+
+    const h1 = document.createElement('h1');
+    h1.innerText = `Hola ${ nombre }`;
+
+    document.body.append( h1 );
+}
