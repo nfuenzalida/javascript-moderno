d8f152eabb666aff0888f62a7edaaffa2ff80516
Nicolas Fuenzalida <na.fuenzalida.contreras@gmail.com>
2022-06-28 12:37:13 -0400

    html loader

--- 03-webpack/webpack.config.js
+++ 03-webpack/webpack.config.js
@@ -1,15 +1,34 @@
+const HtmlWebpack    = require('html-webpack-plugin');
+
 module.exports = {
     mode: "development",
 
+    output: {
+        clean: true
+    },
+
     module: {
         rules: [
-
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            },
         ]
     },
 
     optimization: {},
 
-    plugins: []
+    plugins: [
+        new HtmlWebpack({
+            title: 'Mi Webpack App',
+            // filename: 'index.html',
+            template: './src/index.html'
+        }),
+
+    ]
 }
 
 
