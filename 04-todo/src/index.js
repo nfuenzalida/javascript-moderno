8fb8ab94b105eab5a2bca4af0d86e571db42e03e
Nicolas Fuenzalida <na.fuenzalida.contreras@gmail.com>
2022-06-30 19:00:43 -0400

    construir tareas en html

--- 04-todo/src/index.js
+++ 04-todo/src/index.js
@@ -1,8 +1,11 @@
 import './styles.css';
 import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/components';
 
 const todoList = new TodoList();
 const tarea = new Todo('aprender javascript');
 
 todoList.nuevoTodo(tarea);
 console.log(todoList);
+
+crearTodoHtml(tarea);
--- 04-todo/src/js/components.js
+++ 04-todo/src/js/components.js
@@ -1,16 +1,22 @@
-import '../css/components.css';
-//import webpacklogo from '../assets/img/webpack-logo.png';
 
-export const saludar = ( nombre = 'sin nombre' ) => {
-    console.log('Creando etiqueta h1');
+// Referencias en el HTML
+const divTodoList   = document.querySelector('.todo-list');
 
-    const h1 = document.createElement('h1');
-    h1.innerText = `Hola ${ nombre }`;
+export const crearTodoHtml = (todo) => {
 
-    document.body.append( h1 );
+    const htmlTodo = `
+    <li class="${  (todo.completado) ? 'completed' : '' }" data-id="${ todo.id }">
+        <div class="view">
+            <input class="toggle" type="checkbox" ${  (todo.completado) ? 'checked' : '' }>
+            <label>${ todo.tarea }</label>
+            <button class="destroy"></button>
+        </div>
+        <input class="edit" value="Create a TodoMVC template">
+    </li>`;
 
-    // Img
-    //const img = document.createElement('img');
-    //img.src = webpacklogo;
-    //document.body.append( img );
+    const div = document.createElement('div');
+    div.innerHTML = htmlTodo;
+
+    divTodoList.append(div.firstElementChild);
+    return div.firstElementChild;
 }
