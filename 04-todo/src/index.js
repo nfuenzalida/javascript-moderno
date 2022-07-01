a0906d3606394965045e63ffd3f27f88808b4318
Nicolas Fuenzalida <na.fuenzalida.contreras@gmail.com>
2022-07-01 18:28:05 -0400

    recontruir instancias de todo

--- 04-todo/src/index.js
+++ 04-todo/src/index.js
@@ -1,8 +1,10 @@
 import './styles.css';
-import { Todo, TodoList } from './classes';
-import { crearTodoHtml } from './js/components';
+
+import {Todo, TodoList} from './classes';
+import {crearTodoHtml} from './js/components';
 
 export const todoList = new TodoList();
+
 todoList.todos.forEach(crearTodoHtml);
 
 console.log('todos',todoList.todos);
