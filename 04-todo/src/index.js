7ea28062c1dbac92e77d2116b6bf5363075ddaec
Nicolas Fuenzalida <na.fuenzalida.contreras@gmail.com>
2022-07-01 17:25:56 -0400

    guardando y recuperando todo de localstorage

--- 04-todo/src/index.js
+++ 04-todo/src/index.js
@@ -3,9 +3,6 @@ import { Todo, TodoList } from './classes';
 import { crearTodoHtml } from './js/components';
 
 export const todoList = new TodoList();
-const tarea = new Todo('aprender javascript');
+todoList.todos.forEach(crearTodoHtml);
 
-todoList.nuevoTodo(tarea);
-console.log(todoList);
-
-crearTodoHtml(tarea);
+console.log('todos',todoList.todos);
