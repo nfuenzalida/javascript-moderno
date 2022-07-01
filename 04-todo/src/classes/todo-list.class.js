648a3ebd58270a3b0e97970e1d8a10a6fa966a04
Nicolas Fuenzalida <na.fuenzalida.contreras@gmail.com>
2022-07-01 16:52:18 -0400

    eliminar un todo

--- 04-todo/src/classes/todo-list.class.js
+++ 04-todo/src/classes/todo-list.class.js
@@ -1,3 +1,5 @@
+import { Todo } from './todo.class';
+
 export class TodoList {
 
     constructor() {
@@ -9,7 +11,7 @@ export class TodoList {
     }
 
     eliminarTodo(id) {
-
+        this.todos = this.todos.filter( todo => todo.id != id )
     }
 
     marcarCompletado(id) {
@@ -22,7 +24,7 @@ export class TodoList {
     }
 
     eliminarCompletados() {
-        
+        this.todos = this.todos.filter( todo => !todo.completado )
     }
 
 }
--- 04-todo/src/js/components.js
+++ 04-todo/src/js/components.js
@@ -4,6 +4,7 @@ import { todoList } from '../index';
 // Referencias en el HTML
 const divTodoList   = document.querySelector('.todo-list');
 const txtInput      = document.querySelector('.new-todo');
+const btnBorrar     = document.querySelector('.clear-completed');
 
 export const crearTodoHtml = (todo) => {
 
@@ -46,5 +47,16 @@ divTodoList.addEventListener('click', (event) => {
     } else if(nombreElemento.includes('button')) { // hay que borrar el todo
         todoList.eliminarTodo(todoId);
         divTodoList.removeChild(todoElemento);
+    }
+});
+
+btnBorrar.addEventListener('click', () => {
+    todoList.eliminarCompletados();
+
+    for( let i = divTodoList.children.length-1; i >= 0; i-- ) {
+        const elemento = divTodoList.children[i];
+        if( elemento.classList.contains('completed') ){
+            divTodoList.removeChild(elemento);
+        }
     }
 });
