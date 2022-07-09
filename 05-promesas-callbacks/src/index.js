42db241f18da37e55e63466c41cd355209beae29
Nicolas Fuenzalida <na.fuenzalida.contreras@gmail.com>
2022-07-09 12:50:34 -0400

    promises all

--- 05-promesas-callbacks/src/index.js
+++ 05-promesas-callbacks/src/index.js
@@ -2,9 +2,15 @@ import './styles.css';
 import {buscarHeroe as buscarHeroeCallbacks} from './js/callbacks';
 import {buscarHeroe} from './js/promesas';
 
-const heroeId = 'capi';
+const heroeId1 = 'capi';
+const heroeId2 = 'iron';
 
 
-buscarHeroe(heroeId).then(heroe => {
+buscarHeroe(heroeId1).then(heroe => {
     console.log(heroe);
+});
+
+Promise.all([buscarHeroe(heroeId1),buscarHeroe(heroeId2)])
+    .then(heroes => {
+    console.log('promises.all',heroes);
 });
