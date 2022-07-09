07132bff56c798a08fe50ed3c0202b8a07129806
Nicolas Fuenzalida <na.fuenzalida.contreras@gmail.com>
2022-07-09 12:54:46 -0400

    promises catch y finally

--- 05-promesas-callbacks/src/index.js
+++ 05-promesas-callbacks/src/index.js
@@ -6,11 +6,18 @@ const heroeId1 = 'capi';
 const heroeId2 = 'iron';
 
 
-buscarHeroe(heroeId1).then(heroe => {
+buscarHeroe(heroeId1)
+.then(heroe => {
     console.log(heroe);
+}).catch(err => {
+    alert(err);
 });
 
 Promise.all([buscarHeroe(heroeId1),buscarHeroe(heroeId2)])
-    .then(heroes => {
+.then(heroes => {
     console.log('promises.all',heroes);
+}).catch(err =>{
+    alert(err);
+}).finally( () => {
+    console.log('se termino el promises all');
 });
